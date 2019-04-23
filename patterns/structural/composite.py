db634aabe0fc0945916a411673ef946bb19ed59e
SheiUn <me.sheiun@gmail.com>
2019-04-23 22:52:00 +0800

    Translate composite pattern

--- patterns/structural/composite.py
+++ patterns/structural/composite.py
@@ -2,31 +2,27 @@
 # -*- coding: utf-8 -*-
 
 """
-*What is this pattern about?
-The composite pattern describes a group of objects that is treated the
-same way as a single instance of the same type of object. The intent of
-a composite is to "compose" objects into tree structures to represent
-part-whole hierarchies. Implementing the composite pattern lets clients
-treat individual objects and compositions uniformly.
+*這種模式是什麼？
+Composite 模式描述了一組物件其處理方式與相同類型物件的單一實例相同。
+複合的意圖是將對象“組合”成樹狀結構以表示部分整體層次結構。
+通過實現複合模式，客戶可以統一處理個別物件和組合。
 
-*What does this example do?
-The example implements a graphic class，which can be either an ellipse
-or a composition of several graphics. Every graphic can be printed.
+*這個例子做了什麼？
+該範例實現了一個圖形類別，可以是橢圓形或多個圖形的組合。
+每個圖形都可以被印出。
 
-*Where is the pattern used practically?
-In graphics editors a shape can be basic or complex. An example of a
-simple shape is a line, where a complex shape is a rectangle which is
-made of four line objects. Since shapes have many operations in common
-such as rendering the shape to screen, and since shapes follow a
-part-whole hierarchy, composite pattern can be used to enable the
-program to deal with all shapes uniformly.
+*該模式實際使用在哪裡？
+在圖形編輯器中，形狀是可以基本的或複雜的。簡單形狀的範例是線，
+而複雜形狀是一個由四個線物件組成的矩形。由於形狀具有許多共同的操作，
+例如將形狀渲染到螢幕，且由於形狀遵循部分整體層次結構，
+因此可以使用複合模式使程式能夠統一處理所有形狀。
 
-*References:
+*參考：
 https://en.wikipedia.org/wiki/Composite_pattern
 https://infinitescript.com/2014/10/the-23-gang-of-three-design-patterns/
 
 *TL;DR80
-Describes a group of objects that is treated as a single instance.
+描述一組被視為單一實例的物件。
 """
 
 

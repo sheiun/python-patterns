c1f86c65ac65055827ac1991de0563e11de81663
SheiUn <me.sheiun@gmail.com>
2019-04-24 11:06:14 +0800

    Translate

--- patterns/creational/prototype.py
+++ patterns/creational/prototype.py
@@ -2,26 +2,21 @@
 # -*- coding: utf-8 -*-
 
 """
-*What is this pattern about?
-This patterns aims to reduce the number of classes required by an
-application. Instead of relying on subclasses it creates objects by
-copying a prototypical instance at run-time.
+*這種模式是什麼？
+此模式旨在減少應用程式所需的類別數。 它不是依賴於子類別，
+而是通過在運行時複製原型實例來創建物件。
 
-This is useful as it make it easier to derive new kinds of objects,
-when instances of the class have only a few different combinations of
-state, and when instantiation is expensive.
+這很有用，因為當類別的實例只有幾個不同的狀態組合，並且實例化很昂貴時，
+它可以更容易地產生新類型的物件。
 
-*What does this example do?
-When the number of prototypes in an application can vary, it can be
-useful to keep a Dispatcher (aka, Registry or Manager). This allows
-clients to query the Dispatcher for a prototype before cloning a new
-instance.
+*這個例子做了什麼？
+當應用程式中的原型數量可能不同時，保留 Dispatcher（aka，Registry 或 Manager）會很有用。
+這允許客戶端在克隆新實例之前向 Dispatcher 查詢原型。
 
-Below provides an example of such Dispatcher, which contains three
-copies of the prototype: 'default', 'objecta' and 'objectb'.
+下面提供了一個 Dispatcher 的範例，其中包含三個原型副本：'default'，'objecta' 和 'objectb'。
 
 *TL;DR80
-Creates new object instances by cloning prototype.
+通過克隆原型來創建新的物件實例。
 """
 
 
@@ -30,8 +25,8 @@ class Prototype(object):
     value = 'default'
 
     def clone(self, **attrs):
-        """Clone a prototype and update inner attributes dictionary"""
-        # Python in Practice, Mark Summerfield
+        """ 克隆原型並更新內部屬性字典 """
+        # Python 實踐, Mark Summerfield
         obj = self.__class__()
         obj.__dict__.update(attrs)
         return obj
@@ -42,11 +37,11 @@ class PrototypeDispatcher(object):
         self._objects = {}
 
     def get_objects(self):
-        """Get all objects"""
+        """ 獲取所有物件 """
         return self._objects
 
     def register_object(self, name, obj):
-        """Register an object"""
+        """ 註冊一個物件 """
         self._objects[name] = obj
 
     def unregister_object(self, name):
